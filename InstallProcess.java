1e234a610343c964db516375f815eadbcf4cd8d8
Saikat-tech <akramhossainsaikat249@gmail.com>
2025-06-30 19:04:15 +0600

    Add files via upload

---
+++ InstallProcess.java
@@ -0,0 +1,16 @@
+/**
+ * Task No: 01
+ * Task Name: Installation of JAVA and IDE
+ *
+ * Objective:
+ * To set up my Java programming environment by installing the Java Development Kit (JDK)
+ * and an IDE like IntelliJ IDEA.
+ *
+ * Installation Process:
+ * First, I went to the official Oracle website and downloaded the latest version of the JDK
+ * for my operating system. After downloading, I ran the installer and followed the setup instructions.
+ * Once the installation was done, I made sure the JAVA_HOME environment variable was set properly.
+ *
+ * Next, I downloaded IntelliJ from its official website. I installed it by following the default steps
+ * and then opened the IDE. Finally, I set the JDK path inside the IDE so I could start writing and running Java programs.
+ */
